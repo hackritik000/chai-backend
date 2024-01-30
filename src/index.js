8d4c8b49bf7f8079d1a930fa5693b4a97246472f
hackritik000 <hackritik000@gmail.com>
2024-01-30 15:11:40 +0530

    userModel and videoModel

--- src/index.js
+++ src/index.js
@@ -5,7 +5,19 @@ dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(` Server is running at port : ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
 /*
 import express from "express"
 const app = express()
