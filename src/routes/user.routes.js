410b1e286a32982fdd465b0db9da7ebc9e8144ed
hackritik000 <hackritik000@gmail.com>
2024-02-13 20:07:07 +0530

    user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,6 +5,12 @@ import {
   registerUser,
   refreshAccessToken,
   getUserChannelProfile,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvater,
+  updateUserCoverImage,
+  getUserHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -30,8 +36,19 @@ router.route("/login").post(loginUser);
               Secured Routes
  ----------------------------------------
 */
-router.route("/console").post(verifyJWT, getUserChannelProfile);
+
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvater);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getUserHistory);
 
 export default router;
